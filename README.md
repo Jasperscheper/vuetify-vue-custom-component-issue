95fbd2f00a4a3dd24145b338b34ef732f3f8dd8a
Jasper Scheper <jasper.scheper@gmail.com>
2022-06-21 11:02:28 +0200

    first commit

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Building the webcomponent
+```
+npm run build -- --mode development
+yarn build --mode development
+```
+
+### open index.html in /test to reproduce the issue.
+Please serve the files in /dist to reproduce
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+import HelloWorld from './HelloWorld.vue'
+import vuetify from './plugins/vuetify'
+
+import vueCustomElement from 'vue-custom-element';
+Vue.use(vueCustomElement);
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+HelloWorld.vuetify = vuetify;
+
+
+Vue.customElement('hello-world', HelloWorld, {
+  shadow: false,
+})
+
+// new Vue({
+//   vuetify,
+//   render: h => h(HelloWorld)
+// }).$mount('#app')
