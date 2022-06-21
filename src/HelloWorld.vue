fc09d9857cb94a65cc96923a3eecb384cc07d25e
Jasper Scheper <jasper.scheper@gmail.com>
2022-06-21 15:31:17 +0200

    commmit

---
+++ src/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-row>
+      <v-col>
+        <h1>Test</h1>
+        <v-slider ref="slider" />
+        <v-range-slider />
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+
+  import {VApp, VRow, VCol, VSlider, VRangeSlider} from "vuetify/lib";
+
+  export default {
+    components: {
+      VApp, VRow, VCol, VSlider, VRangeSlider,
+    },
+    methods: {
+    }
+  }
+</script>
